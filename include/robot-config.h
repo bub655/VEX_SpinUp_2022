7fb247d9aaedef2d5085e646d272c21269e4d2c3
bub655 <52846238+bub655@users.noreply.github.com>
2022-12-19 14:56:28 -0800

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -3,14 +3,18 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
+extern motor motor_lf;
+extern motor motor_lm;
+extern motor motor_lb;
+extern motor motor_rf;
+extern motor motor_rm;
+extern motor motor_rb;
+extern motor motor_suc;
+extern motor motor_lau;
 extern controller Controller1;
-extern motor MotorLF;
-extern motor MotorRF;
-extern motor MotorLB;
-extern motor MotorRB;
-extern motor MotorSuction;
-extern motor MotorFlick;
-extern motor MotorLaunch;
+extern digital_out indexer;
+extern digital_out expansion;
+extern inertial iner;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
